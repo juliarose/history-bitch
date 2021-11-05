a921def8bd0499b903f46976b28028e8f83f74ee
colors <colors@Julias-MacBook-Air.local>
2021-11-05 01:13:35 -0400

    update

--- svelte/src/main.js
+++ svelte/src/main.js
@@ -3,6 +3,8 @@ import responseJSON from './response.js';
 import { combineDescriptions } from './trades.js';
 
 const trades = combineDescriptions(responseJSON);
+
+console.log(trades[0]);
 const app = new App({
 	target: document.body,
 	props: {
